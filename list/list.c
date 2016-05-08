4798c17def347e14dc0f1414ac390ea947133dd1
LinEmsber <LinEmsber@gmail.com>
2016-05-08 22:37:41 +0800

    node_pop

--- list/list.c
+++ list/list.c
@@ -75,27 +75,14 @@ void list_clear(list_t *list)
 	node_t * current = list->head;
 	pthread_mutex_lock(&(list->mutex));
 
-
-
 	// destroy list's nodes
 	while( current != NULL ){
 		temp = current;
 		current = current -> next;
 		free(temp);
 	}
+
 	pthread_mutex_unlock(&(list->mutex));
-
-	/* Destroy all entries still in list */
-	while((n = shift_entry(list)) != NULL)	{
-		/* if there is a tagged_value_t associated to the entry,
-		* let's free memory also for it */
-		if(e->tagged && e->value)
-		list_destroy_tagged_value_internal((tagged_value_t *)e->value, list->free_value_cb);
-		else if (list->free_value_cb)
-		list->free_value_cb(e->value);
-
-		destroy_entry(e);
-	}
 }
 
 // ====================node====================
@@ -107,37 +94,52 @@ node_t node_create()
 	return node;
 }
 
-/*
- * Free resources allocated for a list_entry_t structure
- * If the entry is linked in a list this routine will also unlink correctly
- * the entry from the list.
- */
-static inline void
-destroy_entry(list_entry_t *entry)
-{
-    long pos;
-    if(entry)
-    {
-        if(entry->list)
-        {
-            /* entry is linked in a list...let's remove that reference */
-            pos = get_entry_position(entry);
-            if(pos >= 0)
-                remove_entry(entry->list, pos);
-        }
-        free(entry);
-    }
-}
-
+// node remove from the list, and free it.
 void node_destory( node_t * node )
 {
 	int pos;
 	if (node){
 		if(node->list){
+
+			pthread_mutex_lock(&(list->mutex));
+
+			// get the position of node on the list
 			pos = get_node_position(node);
 			if(pos >= 0){
 				remove_node(node->list, pos);
 			}
+
+			pthread_mutex_unlock(&(list->mutex));
 		}
 	}
+}
+
+// pop a node from the end of list, or the bottom of the stack.
+node_t *node_pop(list_t *list)
+{
+	node_t *node;
+	pthread_mutex_lock(&(list->mutex));
+
+	node = list -> tail;
+	if (node){
+		list -> tail = node -> prev;
+		if(list -> prev){
+			list->tail->next = NULL;
+		}
+		(list->length)--;
+
+		node->list = NULL;
+		node->prev = NULL;
+		node->next = NULL;
+
+		if (list->curr == node){
+			list->curr = NULL;
+		}
+	}
+	if (list->length == 0){
+		list -> tail = list -> head = NULL;
+	}
+
+	pthread_mutex_unlock(&(list->mutex));
+	return node;
 }
