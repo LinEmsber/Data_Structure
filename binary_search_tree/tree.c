671a2192cb4761365baf5f2b201ac2bc06c424d5
LinEmsber <LinEmsber@gmail.com>
2016-05-09 23:41:55 +0800

    define structure

---
+++ binary_search_tree/tree.c
@@ -0,0 +1,7 @@
+/* A multi-thread safe linked list
+ * File: tree.c
+ * Author: Lin Emsber
+ * Create Date: 2016, May, 3
+ */
+
+#include "tree.h"
---
+++ binary_search_tree/tree.h
@@ -0,0 +1,31 @@
+/* A multi-thread safe linked list
+ * File: tree.c
+ * Author: Lin Emsber
+ * Create Date: 2016, May, 3
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+// ====================typedef====================
+typedef struct bst_node bst_node_t
+typedef struct bst_tree bst_tree_t
+
+typedef void (*free_cb_t) (void*);
+
+
+// ====================struct====================
+struct bst_node {
+
+	int value;
+	void *data;
+	struct bst_node *left;
+	struct bst_node *right;
+};
+
+struct bst_tree {
+    struct bst_node *root;
+    int count;
+};
