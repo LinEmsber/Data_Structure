8c05faa6f97e7b89c988238fc62b99ac0e1d5e62
LinEmsber <LinEmsber@gmail.com>
2016-05-18 23:26:12 +0800

    add hash_table.h

---
+++ hash_table/hash_table.h
@@ -0,0 +1,26 @@
+/* A hash table
+ * File: list.h
+ * Author: Lin Emsber
+ * Create Date: 2016, May, 18
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// ====================typedef====================
+
+typedef struct entry_s entry_t;
+typedef struct hash_table_s hash_table_t;
+
+// ====================struct====================
+
+struct entry_s{
+	char *key;
+	char *value;
+	struct entry_s *next;
+}
+
+struct hash_table_s{
+	int size;
+	struct entry_s **table;
+}
