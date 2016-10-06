fdcf9f55f21efdb01fa49076dc2389efdb9835e6
LinEmsber <LinEmsber@gmail.com>
2016-10-06 23:01:02 +0800

    fix description, and fix data type

--- binary_search_tree/tree.c
+++ binary_search_tree/tree.c
@@ -1,4 +1,4 @@
-/* A multi-thread safe linked list
+/* A binary search tree
  * File: tree.c
  * Author: Lin Emsber
  * Create Date: 2016, May, 3
@@ -35,21 +35,21 @@ int compare_number(int original_value, int new_value)
 
 // ==================tree==================
 // To create a new binary search tree.
-bst_tree_t *tree_create()
+tree_t *tree_create()
 {
-	bst_tree_t *tree = (bst_tree_t*) malloc (sizeof(bst_tree_t));
+	tree_t *tree = (tree_t*) malloc (sizeof(tree_t));
 	return tree;
 }
 
 // To initialize binary search tree.
-void tree_init( bst_tree_t *tree)
+void tree_init( tree_t *tree)
 {
 	tree -> root = NULL;
 	tree -> count = 0;
 }
 
 // To remove the nodes from a tree, including free the memory of this node.
-void tree_clear_node( bst_tree_t *tree)
+void tree_clear_node( tree_t *tree)
 {
 	node_free(tree->root);
 	tree -> root = NULL;
@@ -57,28 +57,28 @@ void tree_clear_node( bst_tree_t *tree)
 }
 
 // To deallocate a binary search tree, including remove its all nodes.
-void tree_remove( bst_tree_t *tree)
+void tree_remove( tree_t *tree)
 {
 	tree_clear_node(tree);
 	free(tree);
 }
 
 // To return the count of the tree.
-int tree_count(bst_tree_t *tree)
+int tree_count(tree_t *tree)
 {
 	return tree -> count;
 }
 
 // ==================node==================
 // To create a node of binary search tree.
-bst_node_t *node_create ()
+node_t *node_create ()
 {
-	bst_node_t *node = (bst_node_t *)malloc(sizeof(bst_node_t));
+	node_t *node = (node_t *)malloc(sizeof(node_t));
 	return node;
 }
 
 // To free the hierarchical nodes(children) of a root/node(parent).
-void node_free( bst_node_t *root)
+void node_free( node_t *root)
 {
 	if (root != 0){
 		node_free(root->left);
@@ -88,7 +88,7 @@ void node_free( bst_node_t *root)
 }
 
 // To initialize a node.
-bst_node_t *node_init( bst_node_t*node)
+node_t *node_init( node_t*node)
 {
 	node -> value = 0;
 	node -> data = NULL;
@@ -98,14 +98,14 @@ bst_node_t *node_init( bst_node_t*node)
 }
 
 // To input the value to a node.
-bst_node_t *node_input_value(bst_node_t*node, int input_value)
+node_t *node_input_value(node_t*node, int input_value)
 {
 	node -> value = input_value;
 	return node;
 }
 
 // To input the data to a node.
-bst_node_t *node_input_data(bst_node_t*node, void* input_data)
+node_t *node_input_data(node_t*node, void* input_data)
 {
 	node -> data = input_data;
 	return node;
@@ -114,7 +114,7 @@ bst_node_t *node_input_data(bst_node_t*node, void* input_data)
 
 // ==================node add==================
 // A node be added a node with value.
-bst_node_t *node_add_node( bst_node_t *node, int input_value)
+node_t *node_add_node( node_t *node, int input_value)
 {
 	if (node == NULL){
 		node = node_create();
@@ -143,7 +143,7 @@ bst_node_t *node_add_node( bst_node_t *node, int input_value)
 }
 
 // A tree be added a node with value.
-bst_tree_t *tree_add_node( bst_tree_t *tree, int input_value)
+tree_t *tree_add_node( tree_t *tree, int input_value)
 {
 	tree -> root = node_add_node(tree->root, input_value);
 	(tree -> count) ++;
@@ -154,7 +154,7 @@ bst_tree_t *tree_add_node( bst_tree_t *tree, int input_value)
 
 // ==================left most==================
 // To return the left most node from a node.
-bst_node_t *node_left_most( bst_node_t *node)
+node_t *node_left_most( node_t *node)
 {
 	while( node != NULL){
 		if (node -> left == NULL){
@@ -166,16 +166,16 @@ bst_node_t *node_left_most( bst_node_t *node)
 }
 
 // To return the left most node from a node.
-bst_tree_t *tree_left_most(bst_tree_t *tree)
+tree_t *tree_left_most(tree_t *tree)
 {
-	bst_node_t *node = tree->root;
+	node_t *node = tree->root;
 	return node_left_most(node);
 }
 
 
 // ==================remove node==================
 // To remove the left most node.
-void node_remove_left_most(bst_node_t *node)
+void node_remove_left_most(node_t *node)
 {
 	if (node != NULL){
 
@@ -192,7 +192,7 @@ void node_remove_left_most(bst_node_t *node)
 
 
 // To remove a node that is with specific value from the hierarchy of a node.
-bst_node_t *node_remove_node(bst_node_t *node, int input_value)
+node_t *node_remove_node(node_t *node, int input_value)
 {
 	if (node != NULL && is_node_contain_value(node, input_value) == 1 ) {
 
@@ -230,7 +230,7 @@ bst_node_t *node_remove_node(bst_node_t *node, int input_value)
 }
 
 // To remove a node that is with specific value from a tree.
-bst_tree_t *tree_remove_node(bst_tree_t *tree, int input_value)
+tree_t *tree_remove_node(tree_t *tree, int input_value)
 {
 	node_remove_node(tree->root, input_value);
 	(tree->count)--;
@@ -240,7 +240,7 @@ bst_tree_t *tree_remove_node(bst_tree_t *tree, int input_value)
 
 // ==================operation==================
 // To check the value is contained in a hierarchy of a node.
-bst_node_t *is_node_contain_value(bst_node_t *node, int iuput_value)
+node_t *is_node_contain_value(node_t *node, int iuput_value)
 {
 	while(1){
 		if ( compare_num(node -> value, iuput_value) == -1 ){
@@ -262,9 +262,9 @@ bst_node_t *is_node_contain_value(bst_node_t *node, int iuput_value)
 }
 
 // To check a tree is contained a node with specific value.
-bst_tree_t *is_tree_contain_value(bst_tree_t *tree, int iuput_value)
+tree_t *is_tree_contain_value(tree_t *tree, int iuput_value)
 {
-	bst_node_t *current;
+	node_t *current;
 	current = tree -> root;
 
 	return is_node_contain_value(current, input_value);
@@ -272,7 +272,7 @@ bst_tree_t *is_tree_contain_value(bst_tree_t *tree, int iuput_value)
 
 // Compute the height of a tree, the number of nodes along the longest path
 // from the root node down to the farthest leaf node.
-int node_height(bst_node_t* node)
+int node_height(node_t* node)
 {
 	if (node == NULL){
 		return 0;
@@ -293,7 +293,7 @@ int node_height(bst_node_t* node)
 
 // ==================breadth first search==================
 // Print nodes at a given level.
-int print_given_level(bst_node_t* root, int level)
+int print_given_level(node_t* root, int level)
 {
 	// check node is exist or not
 	if (root == NULL){
@@ -311,7 +311,7 @@ int print_given_level(bst_node_t* root, int level)
 }
 
 // To print level order traversal a tree
-void print_breadth_first_search(bst_node_t* root)
+void print_breadth_first_search(node_t* root)
 {
 	int h = tree_height(root);
 	int i;
@@ -337,7 +337,7 @@ void print_breadth_first_search(bst_node_t* root)
  */
 
 // The depth first search pre order.
-void DFS_pre_order(bst_node_t *root)
+void DFS_pre_order(node_t *root)
 {
 	int ret = 0;
 
@@ -359,7 +359,7 @@ void DFS_pre_order(bst_node_t *root)
 }
 
 // The depth first search in order.
-void DFS_in_order(bst_node_t *root)
+void DFS_in_order(node_t *root)
 {
 	int ret = 0;
 
@@ -381,7 +381,7 @@ void DFS_in_order(bst_node_t *root)
 }
 
 // The depth first search post order
-int DFS_post_order(bst_node_t *root)
+int DFS_post_order(node_t *root)
 {
 	int ret = 0;
 
--- binary_search_tree/tree.h
+++ binary_search_tree/tree.h
@@ -1,4 +1,4 @@
-/* A multi-thread safe linked list
+/* A binary search tree
  * File: tree.c
  * Author: Lin Emsber
  * Create Date: 2016, May, 3
@@ -10,8 +10,8 @@
 #include <pthread.h>
 
 // ====================typedef====================
-typedef struct bst_node bst_node_t
-typedef struct bst_tree bst_tree_t
+typedef struct bst_node node_t
+typedef struct bst_tree tree_t
 
 typedef void (*free_cb_t) (void*);
 
@@ -37,88 +37,88 @@ int compare_number(int original_value, int new_value);
 
 // ==================tree==================
 // To create a new binary search tree.
-bst_tree_t *tree_create();
+tree_t *tree_create();
 
 // To initialize binary search tree.
-void tree_init( bst_tree_t *tree);
+void tree_init( tree_t *tree);
 
 // To remove the nodes from a tree, including free the memory of this node.
-void tree_clear_node( bst_tree_t *tree);
+void tree_clear_node( tree_t *tree);
 
 // To deallocate a binary search tree, including remove its all nodes.
-void tree_remove( bst_tree_t *tree);
+void tree_remove( tree_t *tree);
 
 // To return the count of the tree.
-int tree_count(bst_tree_t *tree);
+int tree_count(tree_t *tree);
 
 
 // ==================node==================
 // To create a node of binary search tree.
-bst_node_t *node_create ();
+node_t *node_create ();
 
 // To free the hierarchical nodes(children) of a root/node(parent).
-void node_free( bst_node_t *root);
+void node_free( node_t *root);
 
 // To initialize a node.
-bst_node_t *node_init( bst_node_t*node);
+node_t *node_init( node_t*node);
 
 // To input the value to a node.
-bst_node_t *node_input_value(bst_node_t*node, int input_value);
+node_t *node_input_value(node_t*node, int input_value);
 
 
 // ==================node add==================
 // A node be added a node with value.
-bst_node_t *node_add_node( bst_node_t *node, int input_value);
+node_t *node_add_node( node_t *node, int input_value);
 
 // A tree be added a node with value.
-bst_tree_t *tree_add_node( bst_tree_t *tree, int input_value);
+tree_t *tree_add_node( tree_t *tree, int input_value);
 
 
 // ==================left most==================
 // To return the left most node from a node.
-bst_node_t *node_left_most( bst_node_t *node);
+node_t *node_left_most( node_t *node);
 
 // To return the left most node from a node.
-bst_tree_t *tree_left_most(bst_tree_t *tree);
+tree_t *tree_left_most(tree_t *tree);
 
 
 // ==================remove node==================
 // To remove the left most node.
-void node_remove_left_most(bst_node_t *node);
+void node_remove_left_most(node_t *node);
 
 // To remove a node that is with specific value from the hierarchy of a node.
-bst_node_t *node_remove_node(bst_node_t *node, int input_value);
+node_t *node_remove_node(node_t *node, int input_value);
 
 // To remove a node that is with specific value from a tree.
-bst_tree_t *tree_remove_node(bst_tree_t *tree, int input_value);
+tree_t *tree_remove_node(tree_t *tree, int input_value);
 
 
 // ==================operation==================
 // To check the value is contained in a hierarchy of a node.
-bst_node_t *is_node_contain_value(bst_node_t *node, int iuput_value);
+node_t *is_node_contain_value(node_t *node, int iuput_value);
 
 // To check a tree is contained a node with specific value.
-bst_tree_t *is_tree_contain_value(bst_tree_t *tree, int iuput_value);
+tree_t *is_tree_contain_value(tree_t *tree, int iuput_value);
 
 // Compute the height of a tree, the number of nodes along the longest path
 // from the root node down to the farthest leaf node.
-int node_height(bst_node_t* node);
+int node_height(node_t* node);
 
 
 // ==================breadth first search==================
 // Print nodes at a given level.
-int print_given_level(bst_node_t* root, int level);
+int print_given_level(node_t* root, int level);
 
 // To print level order traversal a tree
-void print_breadth_first_search(bst_node_t* root);
+void print_breadth_first_search(node_t* root);
 
 
 // ==================depth first search==================
 // The depth first search pre order.
-void DFS_pre_order(bst_node_t *root);
+void DFS_pre_order(node_t *root);
 
 // The depth first search in order.
-void DFS_in_order(bst_node_t *root);
+void DFS_in_order(node_t *root);
 
 // The depth first search post order
-void DFS_post_order(bst_node_t *root);
+void DFS_post_order(node_t *root);
