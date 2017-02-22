c270639ce2cb1ff888c45201f70cb246b39cd3ec
Lin Emsber <linemsber@gmail.com>
2017-02-22 23:03:42 +0800

    add typedef for int and unsigned int

---
+++ memory_pool/types.h
@@ -0,0 +1,24 @@
+/* A memory pool
+ * File: type.h
+ * Author: Lin Emsber
+ * Create Date: 2017, Feb, 16
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+
+
+typedef signed char             int8_t;
+typedef int                     int32_t;
+
+typedef unsigned char           uint8_t;
+typedef unsigned int            uint32_t;
+
+// typedef __signed__ char s8;
+// typedef unsigned char u8;
+//
+// typedef __signed__ int s32;
+// typedef unsigned int u32;
+
+
+#endif
