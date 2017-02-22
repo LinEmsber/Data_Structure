ffd1db83b56b75e770085f9855e13f27e5b36e23
Lin Emsber <linemsber@gmail.com>
2017-02-22 23:02:19 +0800

    remove _alloc_block_nodes(), add mem_pool_add_block()

--- memory_pool/mem_pool.c
+++ memory_pool/mem_pool.c
@@ -4,7 +4,10 @@
  * Create Date: 2017, Feb, 1
  */
 
-#include "men_pool.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mem_pool.h"
 
 // ========== private functions ==========
 
@@ -12,58 +15,12 @@
 // ========== functions ==========
 
 
-void _alloc_block_nodes(block_size, block_count)
-{
-	mem_block_t * _free_block_ptr     = 0;
-	mem_block_t * _free_block_ptr_end = 0;
-
-	uint32_t _block_size = sizeof(mem_block_t);
-	uint32_t size_count  = _block_size * block_count;
-
-	char * start;
-	char * block_ptr = start = malloc(size_count);
-	char * end = start + size_count;
-
-	// can't malloc more space(s).
-	if(!block_ptr)
-                return;
-
-	while ( block_ptr < end ) {
-		block_ptr += _block_size;
-
-		mem_block_t* block = (mem_block_t*)block_ptr;
-		block->is_block_start  = (block_ptr == start);
-		block->start = 0;
-
-		// if it's not starting block, set previous block's next block to this.
-		if(block_ptr != start)        {
-			((mem_block_t*)(block_ptr - _block_size))->next = block;
-		}
-
-		block->block_size = 0;
-	}
-
-	((mem_block_t*)(end - _block_size))->next = 0;
-
-	if(_free_block_ptr && _free_block_ptr_end) {
-		// there's block pointer(s) before, and still some in the "_free_block_ptr",
-		// concat newer one list to the old one's tail.
-		_free_block_ptr_end->next = (mem_block_t*)start;
-	} else {
-		// otherwise, replace the whole "_free_block_ptr".
-		_free_block_ptr = (mem_block_t*)start;
-	}
-
-	_free_block_ptr_end = (mem_block_t*)(end - _block_size);
-}
-
-
 /* create a memory pool which has block_count number of block, and each of block's size is block_size
  *
  * @block_size: the size of one block
  * @block_count: the number of block
  */
-mem_pool_t *mem_pool_create (uint32_t block_size, uint32_t block_count)
+mem_pool_t * mem_pool_create (uint32_t block_size, uint32_t block_count)
 {
 
 	// create a mem_pool
@@ -82,18 +39,29 @@ mem_pool_t *mem_pool_create (uint32_t block_size, uint32_t block_count)
 	// set the pool's space
 	mp->start = space;
 	mp->end = space + (block_count * block_size);
+	mp->current = space;
 
 	mp->block_size = block_size;
 	mp->block_count = block_count;
 	mp->next = 0;
 
-	// create free blocks
-	while (space < mp->end){
-		space += block_size;
-		_alloc_block(mp->start, block_size, block_count);
-	}
-
 	return mp;
+}
+
+mem_pool_t * mem_pool_add_block (mem_pool_t * mp, uint32_t size)
+{
+	// create a mem_block
+	mem_block_t * mb;
+	mb = malloc ( sizeof ( *mb ) );
+	if ( !mb )
+		return NULL;
+
+	// assign memory for this block
+	mb->mem_block_start = mp->current;
+	mb->mem_block_end = mp->current + size;
+	mp->current = mp->current + size;
+
+	// check
 }
 
 
