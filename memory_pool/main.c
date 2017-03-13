43aeeb1fa7b27aa2a805a23f4ba80bb67c5205fc
Lin Emsber <linemsber@gmail.com>
2017-03-13 23:56:52 +0800

    Add main.c and Makefile
    1. prototype of main.c, include mem_pool.h and types.h
    2. Makefile for main

---
+++ memory_pool/main.c
@@ -0,0 +1,17 @@
+/* main */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mem_pool.h"
+#include "types.h"
+
+
+int main()
+{
+	mem_pool_t * mp_entry;
+
+	mp_entry = mem_pool_create();
+
+	return 0;
+}
