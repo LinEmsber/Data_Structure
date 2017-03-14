dee4f7cfbcc93fb40e512c8f13828a5b28d50dca
Lin Emsber <linemsber@gmail.com>
2017-03-14 21:44:13 +0800

    Add description and add functions test
    1. make sescription more clear.
    2. test functions: mem_pool_create(), mem_pool_add_block(), and mem_pool_remove_block().

--- memory_pool/main.c
+++ memory_pool/main.c
@@ -1,5 +1,11 @@
 /* main */
 
+/* An main function for memory pool.
+ * File: main.c
+ * Author: Lin Emsber
+ * Create Date: 2017, Mar, 13
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,7 +17,23 @@ int main()
 {
 	mem_pool_t * mp_entry;
 
-	mp_entry = mem_pool_create();
+	void *p_0;
+	void *p_1;
+	void *p_2;
+
+	mp_entry = mem_pool_create(1024, 8);
+	printf("mp_entry->remaing_size: %d\n", mp_entry->remaing_size);
+
+
+	p_0 = mem_pool_add_block(mp_entry, 1024);
+	printf("mp_entry->remaing_size: %d\n", mp_entry->remaing_size);
+	p_1 = mem_pool_add_block(mp_entry, 512);
+	printf("mp_entry->remaing_size: %d\n", mp_entry->remaing_size);
+	p_2 = mem_pool_add_block(mp_entry, 1024);
+	printf("mp_entry->remaing_size: %d\n", mp_entry->remaing_size);
+
+	mem_pool_remove_block(mp_entry, p_1);
+
 
 	return 0;
 }
