012e11d376b3e6ea5d59b1cc974a2cf2564d947f
Lin Emsber <linemsber@gmail.com>
2017-03-26 23:22:25 +0800

    Update singly linked list source coder

--- memory_pool/singly_linked_list.h
+++ memory_pool/singly_linked_list.h
@@ -4,12 +4,16 @@
  * Create Date: 2017, May, 22
  */
 
-#include "singly_linked_list.h"
-
-
 #ifndef SINGLY_LIST_H
 #define SINGLY_LIST_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "singly_linked_list.h"
+
+
+// structure
 typedef struct node node_t;
 
 struct node{
@@ -18,11 +22,11 @@ struct node{
 };
 
 
-
+// function
 node_t * node_create(int val);
-int node_add(node_t * head, node_t * n);
-int node_remove(node_t * head, int target_val);
-int node_free(node_t * head);
+int node_insert(node_t * head, node_t * n);
+int node_delete(node_t * head, int target_val);
+int node_free_all(node_t * head);
 int print_list(node_t * head);
 
 
