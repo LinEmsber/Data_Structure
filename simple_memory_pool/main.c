b5d784fc16362f0caacc42f5dd8903640a2e0a1e
Lin Emsber <linemsber@gmail.com>
2017-04-15 21:03:47 +0800

    Test all functions of memory pool.
    1. Allocate memorys for the all pointers, and save some string in those memory.
    Check those meomory can be used, and print the strings saved in the memory.

--- simple_memory_pool/main.c
+++ simple_memory_pool/main.c
@@ -1,35 +1,58 @@
-/* simple memory pool */
+/* simple memory pool
+ *
+ * USAGE: gcc -Wall -std=c99 main.c simple_mem_pool.c && ./a.out
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "simple_mem_pool.h"
 
+#define BLOCK_NUMS 100
+
 int main()
 {
-        // mem_pool_t * mem_pool_create( size_t size );
-        // void mem_pool_destroy( mem_pool_t *mp );
-        // size_t mem_pool_available( mem_pool_t *mp );
-        // void * mem_pool_alloc( mem_pool_t *mp, size_t size );
+        int i, j;
 
-        mem_pool_t * mp;
-        void * p_1;
-        void * p_2;
-        void * p_3;
+        size_t remaining;
+        mem_pool_t * mp_entry;
+        void * p_block[BLOCK_NUMS];
 
-        mp = mem_pool_create(1024);
+        /* The string for copying into each p_block */
+	char str_for_copy[8] = "ABCDEFG";
 
-        p_1 = mem_pool_alloc(mp, 512);
-        if (p_1)
+
+        /* memory pool create */
+        mp_entry = mem_pool_create(1024);
+        if (!mp_entry)
                 printf("error\n");
 
-        p_2 = mem_pool_alloc(mp, 200);
-        if (p_2)
-                printf("error\n");
+        for (i = 0; i < BLOCK_NUMS; i++){
 
-        // p_3 = mem_pool_alloc(mp, 512);
-        // if (p_3)
-        //         printf("error\n");
+                /* allocate the memory block for every p_block. */
+                p_block[i] = mem_pool_alloc(mp_entry, 8);
+                if (!p_block[i])
+                        printf("error\n");
 
+                /* write some string into p_block[i] */
+                strncpy(p_block[i], str_for_copy, 7);
 
+                for (j = 0; j < 7; j++){
+			str_for_copy[j] ++;
+			str_for_copy[j] = ( (str_for_copy[j] - 65) % 52) + 65;
+		}
+
+		printf("string: %s\n", str_for_copy);
+
+        }
+
+        /* print the remaining space of the memory pool */
+        remaining = mem_pool_remaining_space( mp_entry );
+        printf("The remaining space of memory pool: %lu\n", remaining);
+
+        /* free the memory pool */
+        mem_pool_destroy(mp_entry);
+
+        return 0;
 }
