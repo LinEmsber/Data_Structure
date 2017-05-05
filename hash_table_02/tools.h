013dc8000cfa4c91eba2b86f5a2a42aa784e7f05
Lin Emsber <linemsber@gmail.com>
2017-05-05 23:55:40 +0800

    Add random string generator for creating the keys

--- hash_table_02/tools.h
+++ hash_table_02/tools.h
@@ -1,3 +1,7 @@
+/* tools.h */
+
+#ifndef TOOLS_H
+#define TOOLS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,12 +30,40 @@ char ** string_increament(unsigned int _string_len)
 	return ptr_ptr_string;
 }
 
+
+
+char ** random_string_generator(size_t _string_number, size_t _each_string_length)
+{
+	int i, j;
+	char ** ptr_ptr_string;
+	char string_tmp[_each_string_length];
+
+	ptr_ptr_string = malloc ( sizeof(char *) * _string_number );
+
+	for (i = 0; i < _string_number; i++){
+
+		ptr_ptr_string[i] = malloc (sizeof(char) * _each_string_length);
+
+		for(j = 0; j < _each_string_length - 1; j++) {
+			string_tmp[j] = (rand() % 26) + 'a';
+		}
+		string_tmp[_each_string_length - 1] = '\0';
+
+		strncpy(ptr_ptr_string[i], string_tmp, _each_string_length);
+	}
+
+	return ptr_ptr_string;
+}
+
+
 void free_string(char ** _ptr_ptr_string, unsigned int _string_len)
 {
 	int i;
 
 	for (i = 0; i < _string_len; i++)
-		free(_ptr_ptr_string[i]);
+	free(_ptr_ptr_string[i]);
 
 	free(_ptr_ptr_string);
 }
+
+#endif
