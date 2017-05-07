0e01f3f94b908e4e173b3501c03a3467d9c2ad62
Lin Emsber <linemsber@gmail.com>
2017-05-07 23:22:36 +0800

    Test functions.
    1. Test functions:
            hash_table_create()
            hash_table_put()
            hash_table_get()
            hash_table_remove()
            hash_table_list_keys()
            hash_table_iterate()
            hash_table_iterate_keys()
            hash_table_iterate_datas()
            hash_table_clear()
            hash_table_destroy()
    2. Test large numbers of datas.
    3. Fix some bugs of random string generator.

--- hash_table_02/main.c
+++ hash_table_02/main.c
@@ -1,6 +1,9 @@
 /* The implentation of hash table.
  *
  * USAGE: gcc -Wall -g main.c hash_table.c && ./a.out
+ *
+ * memory and error check: gcc -Wall -g main.c hash_table.c && valgrind -v ./a.out
+ * GDB: gcc -Wall -g main.c hash_table.c && gdbtui ./a.out
  */
 
 #include <stdio.h>
@@ -11,7 +14,7 @@
 #include "hash_table.h"
 #include "tools.h"
 
-#define TABLE_SIZE 10
+#define TABLE_SIZE 4096 * 16
 
 int main()
 {
@@ -19,41 +22,88 @@ int main()
 	srand( (unsigned) time(NULL) );
 
 	void * p;
-	char ** pp_key = string_generator(32, TABLE_SIZE);
+	char * p_key;
+	char * p_data;
+	char ** pp_key = random_string_generator(32, TABLE_SIZE);
 	char ** pp_data = string_increament(TABLE_SIZE);
 	char * pp_key_return[TABLE_SIZE];
 
-
-	/* create, put, and get hash */
+	/* create */
 	hash_table_t * hash_table_entry = hash_table_create(TABLE_SIZE);
 
+	/* put, get and remove */
 	for (i = 0; i < TABLE_SIZE; i++){
+		p = hash_table_put(hash_table_entry, pp_key[i], pp_data[i]);
+		// if (p != NULL)
+		// 	printf("collision\n");
+	}
 
-		hash_table_put(hash_table_entry, pp_key[i], pp_data[i]);
+	for (i = 0; i < TABLE_SIZE; i++){
 		p = hash_table_get(hash_table_entry, pp_key[i]);
 		// printf( "%s\n", (char *)p );
-
 	}
+
+	for (i = 0; i < TABLE_SIZE ; i++){
+		hash_table_remove(hash_table_entry, pp_key[i]);
+	}
+	printf("\n");
+
+
+	/* the collision of hash function */
+	p = hash_table_put(hash_table_entry, "the_first_string", "string_7788");
+	if (p != NULL)
+		printf("collision\n");
+
+	p = hash_table_put(hash_table_entry, "the_second_string", "string_5566");
+	if (p != NULL)
+		printf("collision\n");
+
+	hash_table_remove(hash_table_entry, "the_first_string");
+	hash_table_remove(hash_table_entry, "the_second_string");
 
 
 	/* hash_table_list_key */
+	for (i = 0; i < TABLE_SIZE; i++){
+		hash_table_put(hash_table_entry, pp_key[i], pp_data[i]);
+	}
+
 	hash_table_list_keys(hash_table_entry, pp_key_return, TABLE_SIZE);
 	for (i = 0; i < TABLE_SIZE; i++){
-		printf( "%s\n", (char *)pp_key_return[i] );
+		// printf( "%s\n", (char *)pp_key_return[i] );
 	}
+	printf("\n");
 
 
 	/* iterator */
-
 	hash_elem_iter_t hash_elem_iter_entry = INIT_HASH_TABLE_ITERATOR( hash_table_entry );
 	hash_elem_t * hash_elem_entry = hash_table_iterate( &hash_elem_iter_entry );
 
 	while ( hash_elem_entry != NULL){
-
-		printf("key: %s, data: %s\n", hash_elem_entry->key, (char *) hash_elem_entry->data);
+		// printf("key: %s, data: %s\n", hash_elem_entry->key, (char *) hash_elem_entry->data);
 		hash_elem_entry = hash_table_iterate( &hash_elem_iter_entry );
-
 	}
+	printf("\n");
+
+	/* iterator 2 */
+	hash_elem_iter_t hash_elem_iter_entry_2 = INIT_HASH_TABLE_ITERATOR( hash_table_entry );
+	p_key = hash_table_iterate_keys( &hash_elem_iter_entry_2 );
+
+	while (p_key != NULL){
+		// printf("key: %s\n", p_key);
+		p_key = hash_table_iterate_keys( &hash_elem_iter_entry_2 );
+	}
+	printf("\n");
+
+
+	/* iterator 3 */
+	hash_elem_iter_t hash_elem_iter_entry_3 = INIT_HASH_TABLE_ITERATOR( hash_table_entry );
+	p_data = hash_table_iterate_datas( &hash_elem_iter_entry_3 );
+
+	while ( p_data != NULL){
+		// printf("data: %s\n", p_data);
+		p_data = hash_table_iterate_datas( &hash_elem_iter_entry_3 );
+	}
+	printf("\n");
 
 	/* destory */
 	hash_table_destroy(hash_table_entry);
--- hash_table_02/tools.h
+++ hash_table_02/tools.h
@@ -11,7 +11,7 @@ char ** string_increament(unsigned int _string_len)
 {
 	int i, j;
 	char ** ptr_ptr_string;
-	char string_[8] = "ABCDEFG";
+	char string_[8] = "CKDENSD";
 
 	ptr_ptr_string = (char **) malloc ( sizeof(char*) * _string_len );
 
@@ -21,7 +21,7 @@ char ** string_increament(unsigned int _string_len)
 
 		for (j = 0; j < 7; j++){
 			string_[j] ++;
-			string_[j] = ( (string_[j] - 65) % 52) + 65;
+			string_[j] = ( (string_[j] - 65) % 26) + 65;
 		}
 
 		strcpy(ptr_ptr_string[i], string_);
@@ -32,36 +32,39 @@ char ** string_increament(unsigned int _string_len)
 
 
 
-char ** random_string_generator(size_t _string_number, size_t _each_string_length)
+char ** random_string_generator(size_t _each_string_length, size_t _string_count)
 {
 	int i, j;
 	char ** ptr_ptr_string;
 	char string_tmp[_each_string_length];
 
-	ptr_ptr_string = malloc ( sizeof(char *) * _string_number );
+	ptr_ptr_string = malloc ( sizeof(char *) * _string_count );
 
-	for (i = 0; i < _string_number; i++){
+	for (i = 0; i < _string_count; i++){
 
+		/* create the memory space for each string */
 		ptr_ptr_string[i] = malloc (sizeof(char) * _each_string_length);
 
+		/* generate the random string */
 		for(j = 0; j < _each_string_length - 1; j++) {
 			string_tmp[j] = (rand() % 26) + 'a';
 		}
 		string_tmp[_each_string_length - 1] = '\0';
 
+		/* copy the random string to the target string */
 		strncpy(ptr_ptr_string[i], string_tmp, _each_string_length);
 	}
 
 	return ptr_ptr_string;
 }
 
-
-void free_string(char ** _ptr_ptr_string, unsigned int _string_len)
+void free_string(char ** _ptr_ptr_string, size_t _string_count)
 {
 	int i;
 
-	for (i = 0; i < _string_len; i++)
-	free(_ptr_ptr_string[i]);
+	for (i = 0; i < _string_count; i++){
+		free(_ptr_ptr_string[i]);
+	}
 
 	free(_ptr_ptr_string);
 }
