0968bb13dc48b5ce22ca0e08c01cd564f044b00d
Lin Emsber <linemsber@gmail.com>
2018-06-27 23:37:55 +0800

    Review

--- graph/graph_01.c
+++ graph/graph_01.c
@@ -1,72 +1,76 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct edge{
+struct Edge{
 	int vertexIndex;
-	struct edge * edgePtr;
+	struct Edge * edge_ptr;
 }edge;
 
 struct vertex{
-	int vertexKey;
-	struct edge * edgePtr;
+	int vertex_key;
+	struct Edge * edge_ptr;
 }vertex;
 
 struct vertex graph[10];
 int vertexCount = 0;
 
-void insert_vertex(int vertexKey)
+void insert_vertex(int vertex_key)
 {
-	graph[vertexCount].vertexKey=vertexKey;
-	graph[vertexCount].edgePtr=NULL;
+	graph[vertexCount].vertex_key = vertex_key;
+	graph[vertexCount].edge_ptr = NULL;
 	vertexCount++;
 }
 
 void insert_edge(int vertex1, int vertex2)
 {
-	struct edge *e,*e1,*e2;
-	e = graph[vertex1].edgePtr;
+	struct Edge *e, *e1, *e2;
+	e = graph[vertex1].edge_ptr;
 
-	while(e && e->edgePtr) {
-		e=e->edgePtr;
+	while(e && e->edge_ptr) {
+		e = e->edge_ptr;
 	}
 
-	e1 = (struct edge *) malloc( sizeof(*e1) );
+	e1 = (struct Edge *) malloc( sizeof(*e1) );
 	e1->vertexIndex = vertex2;
-	e1->edgePtr = NULL;
+	e1->edge_ptr = NULL;
 
-	if(e)
-		e->edgePtr = e1;
-	else
-		graph[vertex1].edgePtr = e1;
-
-	e=graph[vertex2].edgePtr;
-
-	while(e && e->edgePtr) {
-		e = e->edgePtr;
+	if(e){
+		e->edge_ptr = e1;
+	} else {
+		graph[vertex1].edge_ptr = e1;
 	}
 
-	e2 = (struct edge *)malloc( sizeof(*e2) );
-	e2->vertexIndex = vertex1;
-	e2->edgePtr = NULL;
+	e = graph[vertex2].edge_ptr;
 
-	if(e)
-		e->edgePtr = e2;
-	else
-		graph[vertex2].edgePtr = e2;
+	while(e && e->edge_ptr) {
+		e = e->edge_ptr;
+	}
+
+	e2 = (struct Edge *) malloc( sizeof(*e2) );
+	e2->vertexIndex = vertex1;
+	e2->edge_ptr = NULL;
+
+	if(e){
+		e->edge_ptr = e2;
+	} else {
+		graph[vertex2].edge_ptr = e2;
+	}
 }
 
 void print_graph()
 {
 	int i;
-	struct edge *e;
-	for( i = 0; i < vertexCount; i++ )
-	{
-		printf("%d(%d)", i, graph[i].vertexKey);
-		e = graph[i].edgePtr;
+	struct Edge * e;
+
+	for( i = 0; i < vertexCount; i++ ) {
+		printf("%d(%d)", i, graph[i].vertex_key);
+		e = graph[i].edge_ptr;
+
 		while(e) {
 			printf("->%d", e->vertexIndex);
-			e = e->edgePtr;
+			e = e->edge_ptr;
 		}
+
 		printf("\n");
 	}
 }
