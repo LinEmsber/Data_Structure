46d6dbe9363aaf1811e747a8ddc1c37b7bca58e3
Lin Emsber <linemsber@gmail.com>
2018-06-29 00:39:51 +0800

    Improve algorithm and rename element of Edge

--- graph_theory/graph_01.c
+++ graph_theory/graph_01.c
@@ -1,82 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/* Structure of Edge and Vertex. */
 struct Edge{
-	int vertexIndex;
-	struct Edge * edge_ptr;
-}edge;
+	int Vertex_index;
+	struct Edge * next;
+};
 
-struct vertex{
+struct Vertex{
 	int vertex_key;
 	struct Edge * edge_ptr;
-}vertex;
+};
 
-struct vertex graph[10];
-int vertexCount = 0;
+struct Vertex graph[10];
+int vertex_count = 0;
 
 void insert_vertex(int vertex_key)
 {
-	graph[vertexCount].vertex_key = vertex_key;
-	graph[vertexCount].edge_ptr = NULL;
-	vertexCount++;
+	graph[vertex_count].vertex_key = vertex_key;
+	graph[vertex_count].edge_ptr = NULL;
+	vertex_count++;
 }
 
-void insert_edge(int vertex1, int vertex2)
+/* Construct the connection between vertex_1 and vertex_2. */
+void insert_edge(int vertex_1, int vertex_2)
 {
-	struct Edge *e, *e1, *e2;
-	e = graph[vertex1].edge_ptr;
+	struct Edge * e_tmp;
 
-	while(e && e->edge_ptr) {
-		e = e->edge_ptr;
-	}
+	struct Edge * e_1 = (struct Edge *) malloc( sizeof(*e_1) );
+	e_1->Vertex_index = vertex_2;
+	e_1->next = NULL;
 
-	e1 = (struct Edge *) malloc( sizeof(*e1) );
-	e1->vertexIndex = vertex2;
-	e1->edge_ptr = NULL;
+	/* Start from the first edge to find the last edge of vertex. */
+	e_tmp = graph[vertex_1].edge_ptr;
+	while(e_tmp && e_tmp->next)
+		e_tmp = e_tmp->next;
 
-	if(e){
-		e->edge_ptr = e1;
+	/* If the e_tmp is NULL means that it is the first edge of this vertex. */
+	if(e_tmp){
+		e_tmp->next = e_1;
 	} else {
-		graph[vertex1].edge_ptr = e1;
+		graph[vertex_1].edge_ptr = e_1;
 	}
 
-	e = graph[vertex2].edge_ptr;
+	e_tmp = graph[vertex_2].edge_ptr;
 
-	while(e && e->edge_ptr) {
-		e = e->edge_ptr;
-	}
+	struct Edge * e_2 = (struct Edge *) malloc( sizeof(*e_2) );
+	e_2->Vertex_index = vertex_1;
+	e_2->next = NULL;
 
-	e2 = (struct Edge *) malloc( sizeof(*e2) );
-	e2->vertexIndex = vertex1;
-	e2->edge_ptr = NULL;
+	while(e_tmp && e_tmp->next)
+	e_tmp = e_tmp->next;
 
-	if(e){
-		e->edge_ptr = e2;
+	if(e_tmp){
+		e_tmp->next = e_2;
 	} else {
-		graph[vertex2].edge_ptr = e2;
+		graph[vertex_2].edge_ptr = e_2;
 	}
 }
 
 void print_graph()
 {
 	int i;
-	struct Edge * e;
+	struct Edge * e_tmp;
 
-	for( i = 0; i < vertexCount; i++ ) {
-		printf("%d(%d)", i, graph[i].vertex_key);
-		e = graph[i].edge_ptr;
+	for( i = 0; i < vertex_count; i++ ) {
 
-		while(e) {
-			printf("->%d", e->vertexIndex);
-			e = e->edge_ptr;
+		printf("%d (%d)", i, graph[i].vertex_key);
+		e_tmp = graph[i].edge_ptr;
+
+		while(e_tmp) {
+			printf(" -> %d", e_tmp->Vertex_index);
+			e_tmp = e_tmp->next;
 		}
 
 		printf("\n");
 	}
 }
 
-main()
+int main()
 {
+
 	insert_vertex(5);
 	insert_vertex(3);
 	insert_vertex(4);
@@ -87,6 +91,8 @@ main()
 	insert_edge(0, 2);
 	insert_edge(1, 3);
 	insert_edge(1, 4);
+
+	printf("vertex count: %d\n\n", vertex_count);
 
 	print_graph();
 
