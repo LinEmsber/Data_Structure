878c3ca998fa400b0d65fee7378b7a3090786115
Lin Emsber <linemsber@gmail.com>
2018-07-17 23:04:54 +0800

    The basic operation of queue

--- graph_03/list.c
+++ graph_03/list.c
@@ -19,7 +19,39 @@ node_t * create_node(int _value)
         return new_node;
 }
 
-void list_insert_head(list_t * list, node_t * node)
+void list_head_insert_node(list_t * list, node_t * node)
 {
-        
+
+        if (list->length == 0){
+                list->head = list->tail = node;
+        } else {
+                node_t * tmp = list->head;
+                list->head->prev = node;
+
+                node->next = tmp;
+                node->prev = NULL;
+
+                list->head = node;
+        }
+
+        (list->length)++;
+}
+
+node_t * list_tail_pop_node(list_t * list)
+{
+        node_t * tmp = list->tail;
+
+        if (tmp){
+                if (list->head == list->tail) {
+                        list->head = list->tail = NULL;
+                } else {
+                        list->tail = n->prev;
+                        list->tail->next = NULL;
+                }
+
+                (list->length)--;
+                // tmp->prev = tmp->tail = NULL/
+        }
+
+        return tmp;
 }
